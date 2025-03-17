2ec8c3bad88a6f84bdbad261e31523bb3fa845d3
Alejandro <aletomasperren@gmail.com>
2025-03-17 15:40:32 -0300

    Modelos, tipado, y respuestas en JSON

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from fastapi import FastAPI, Body
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Path, Query
+from fastapi.responses import HTMLResponse, JSONResponse
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI(
     title='Crud FastApi',
     description='Una API Sencilla',
     version='0.0.1',
 )
+
+class Movie(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(default='Titulo de la pelicula', min_length=3, max_length=60)
+    overview: str = Field(default='Descripcion de la pelicula', min_length=15, max_length=90)
+    year: int = Field(default=2023)
+    rating: float = Field(ge=1, le=10)
+    category: str = Field(default='Categoria de la pelicula', min_length=3, max_length=60)
+    
 
 movies = []
 
@@ -15,45 +26,35 @@ def read_root():
 
 @app.get('/movies', tags=['Movies'])
 def get_movie():
-    return movies
+    return JSONResponse(content=movies)
+
+@app.get('/movies/{id}', tags=['Movies'])
+def get_movie(id: int = Path(ge=1, le=100)):
+    for item in movies:
+        if item.id == id:
+            return JSONResponse(content=item.model_dump())
+    return JSONResponse(content={"error": "Movie not found"}, status_code=404)
+
+@app.get('/movies/', tags=['Movies'])
+def get_movies_by_category(category: str = Query(min_length=3, max_length=15)):
+    return category
 
 @app.post('/movies', tags=['Movies'])
-def create_movie(
-    id: int = Body(), 
-    title: str = Body(), 
-    overview: str = Body(),
-    year: int = Body(),
-    rating: float = Body(),
-    category: str = Body()
-):
-    new_movie = {
-        "id": id,
-        "title": title,
-        "overview": overview,
-        "year": year,
-        "rating": rating,
-        "category": category
-    }
+def create_movie(movie: Movie):
+    new_movie = movie
     movies.append(new_movie)
-
-    return new_movie  
+    return JSONResponse(status_code=201 ,content={'message': 'Se agrego una nueva pelicula', 'movies': [m.model_dump() for m in [new_movie]]}) 
 
 @app.put("/movies/{id}", tags=['Movies'])
-def update_movie(
-    id: int, 
-    title: str = Body(), 
-    overview: str = Body(),
-    year: int = Body(),
-    rating: float = Body(),
-    category: str = Body()
-):
+def update_movie(id: int, movie: Movie):
     for item in movies:
         if item["id"] == id:
-            item['overview'] = overview
-            item['year'] = year
-            item['rating'] = rating
-            item['category'] = category
-            return item 
+            item['title'] = movie.title
+            item['overview'] = movie.overview
+            item['year'] = movie.year
+            item['rating'] = movie.rating
+            item['category'] = movie.category,
+            return JSONResponse(content={'message': 'Se ha modificado la pelicula'})
     return {"error": "Movie not found"} 
 
 @app.delete('/movies/{id}', tags=['Movies'])
@@ -61,5 +62,5 @@ def delete_movie(id: int):
     for item in movies:
         if item['id'] == id:
             movies.remove(item)
-            return {"message": "Movie deleted successfully"} 
+            return JSONResponse(content={"message": "Movie deleted successfully"}) 
     return {"error": "Movie not found"}  
